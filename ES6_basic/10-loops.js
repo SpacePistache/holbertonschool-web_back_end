90c178be11ceb41e47955113604a31533ce93574
SpacePistache <lukeholley@hotmail.com>
2025-03-25 13:40:45 +0100

    removed ind counter

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const idx = 0;
   for (const idx of array) {
     const value = array[idx];
     array[idx] = appendString + value;
   }
 
-  return array;
+  return idx;
 }
