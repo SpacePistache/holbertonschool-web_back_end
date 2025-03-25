fd3ae611b82a419f0a9ee445fe373522d32f7d5a
SpacePistache <lukeholley@hotmail.com>
2025-03-25 09:38:27 +0100

    return added return

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,2 +1,3 @@
-export default function returnHowManyArguments (...returnHowManyArguments) {
+export default function returnHowManyArguments(...returnHowManyArguments) {
+  return returnHowManyArguments;
 }
