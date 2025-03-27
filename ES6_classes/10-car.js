86b876b207ce66c02f601097979942c8274872aa
SpacePistache <lukeholley@hotmail.com>
2025-03-27 17:25:43 +0100

    added export line at end of file

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,12 +1,13 @@
 class Car {
   constructor(brand, motor, color) {
-	this._brand = brand;
-	this.color = color;
-	this.motor = motor;
+    this._brand = brand;
+    this.color = color;
+    this.motor = motor;
   }
 
   cloneCar() {
-	const ClonedCar = Object.getPrototypeOf(this).constructor;
-	return new ClonedCar(this._brand, this._motor, this._color);
+    const ClonedCar = Object.getPrototypeOf(this).constructor;
+    return new ClonedCar(this._brand, this._motor, this._color);
   }
 }
+export default Car;
