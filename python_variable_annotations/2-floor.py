4b7e8e9050375309679202ea7f691a59e738101d
SpacePistache <lukeholley@hotmail.com>
2025-04-24 10:54:40 +0200

    floor function first commit

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+A module employing the floor function rounding down to an int
+"""
+import math
+
+
+def get_floored(a: float) -> int:
+    """
+        Return: the number rounded down from float to int.
+
+        Args: a (float) the number to round down or 'floor'
+
+    """
+    return math.floor(a)
