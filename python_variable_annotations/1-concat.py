4c52fd8c4f2212e0511599c94fed07066b248af7
SpacePistache <lukeholley@hotmail.com>
2025-04-24 10:38:01 +0200

    first commit

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A module for conactenating two strings. Via a super special function!
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenating two strings and returning the result
+       Args:
+            str1 (str) first string
+            str2 (str) second string
+
+        returns:
+                the concatenated strings together.
+    """
+    return str1 + str2
