a8132e8556c69d84f00f2bcc1f2b668346314277
SpacePistache <lukeholley@hotmail.com>
2025-04-24 13:41:16 +0200

    first commit

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Additions list of ints and floats and returns their sum"""
+
+
+def sum_mixed_list(mxd_list: int, float) -> float:
+    """Additions list of floats and ints returns float
+
+    Args:
+        mxd_list (list[float, int]): list of float and int to addition.
+
+    Return:
+        float: The sum of ints and floats.
+    """
+    return sum_mixed_list(float)
