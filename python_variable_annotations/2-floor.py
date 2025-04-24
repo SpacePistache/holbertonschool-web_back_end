ffaa858c5278b0211fcf541b897789509668a5b7
SpacePistache <lukeholley@hotmail.com>
2025-04-24 10:55:58 +0200

    not enough annotation

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -10,6 +10,8 @@ def get_floored(a: float) -> int:
         Return: the number rounded down from float to int.
 
         Args: a (float) the number to round down or 'floor'
+        
+        Returns: (int) the number that has been rounded down.
 
     """
     return math.floor(a)
