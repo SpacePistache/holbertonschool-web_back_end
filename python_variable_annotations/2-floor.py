2594490d737eaa5e7448dbde775c8e95ebe78ce3
SpacePistache <lukeholley@hotmail.com>
2025-04-24 11:02:26 +0200

    stripped back version deleted superfluous info and import

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,18 +2,7 @@
 """
 A module employing the floor function rounding down to an int
 """
-import math
-
-floor = __import__('2-floor').floor
 
 
-def get_floored(a: float) -> int:
-    """
-        Return: the number rounded down from float to int.
-
-        Args: a (float) the number to round down or 'floor'
-        
-        Returns: (int) the number that has been rounded down.
-
-    """
+def floor(a: float) -> int:
     return int(a)
