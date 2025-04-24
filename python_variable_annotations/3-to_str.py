c0146a7fb5532bf19f853c4b1feab70485614abf
SpacePistache <lukeholley@hotmail.com>
2025-04-24 11:16:21 +0200

    first commit

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module transforms float into string format.
+"""
+
+
+def to_str(n):
+    """Argument: float (n)
+
+       Returns: String (n)
+    """
+
+    return str(n)
