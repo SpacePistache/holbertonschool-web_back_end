07ae533ea3f58742135f86d9b2cc0292f255a91d
SpacePistache <lukeholley@hotmail.com>
2025-04-29 14:04:38 +0200

    first commit

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,14 +3,11 @@
 comprehensing over async_generator,
 then return the 10 random numbers.
 """
-import asyncio
-import random
-from typing import AsyncGenerator
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+from typing import List
+async_generator = __import__('0-async_generator').async_comprehension
 
 
-async def async_comprehension() -> AsyncGenerator[float, None]:
+async def async_comprehension() -> List[float]:
     """Loops x1 and returns x10 rand int"""
-    for _ in range(10):
-        yield random.randint(0, 10)
-        asyncio.sleep(1)
+    [i async for i in async_generator()]
