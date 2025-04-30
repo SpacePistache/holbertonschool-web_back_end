3c7bed90c8b2a4ffc0178c77388da5fa91ba789a
SpacePistache <lukeholley@hotmail.com>
2025-04-30 14:48:02 +0200

    added method notation

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -39,6 +39,7 @@ class Server:
         return self.dataset()[start_index:end_index]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Returns dictionary of key value pairs"""
         data = self.get_page(page, page_size)
         total_items = len(self.dataset())
         total_pages = math.ceil(total_items/page_size)
