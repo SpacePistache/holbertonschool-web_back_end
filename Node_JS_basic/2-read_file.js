9a43f63bced09c63217639e80339e81df17b8b5f
SpacePistache <lukeholley@hotmail.com>
2025-05-23 13:12:40 +0200

    eslint corrections

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -8,22 +8,19 @@ function countStudents(path) {
     throw new Error('Cannot load the database');
   }
 
-  const lines = data.split('\n').filter(line => line.trim() !== '');
+  const lines = data.split('\n').filter((line) => line.trim() !== '');
 
   const headers = lines[0].split(',');
 
   const fieldIdx = headers.indexOf('field');
   const firstnameIdx = headers.indexOf('firstname');
 
-  const students = lines.slice(1).map(line => line.split(','));
-
+  const students = lines.slice(1).map((line) => line.split(','));
 
   const fields = {};
 
-  students.forEach(student => {
-
+  students.forEach((student) => {
     if (student.length < headers.length) return;
-
     const field = student[fieldIdx];
     const firstname = student[firstnameIdx];
 
@@ -33,8 +30,7 @@ function countStudents(path) {
     fields[field].push(firstname);
   });
 
-
-  const totalStudents = students.filter(student => student.length === headers.length).length;
+  const totalStudents = students.filter((student) => student.length === headers.length).length;
   console.log(`Number of students: ${totalStudents}`);
 
   for (const [field, firstnames] of Object.entries(fields)) {
