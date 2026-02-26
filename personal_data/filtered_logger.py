8b5f3c21d2e17a463d6a4fec88ddcd98618b4679
SpacePistache <lukeholley@hotmail.com>
2026-02-26 10:00:24 +0100

    task 0 corrected pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,9 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-	""" Function to select and return key information"""
-	filtered_data = rf"({'|'.join(fields)})=[^{separator}]*"
-	return re.sub(filtered_data, rf"\1={redaction}", message)
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Function to select and return key information"""
+    filtered_data = rf"({'|'.join(fields)})=[^{separator}]*"
+    return re.sub(filtered_data, rf"\1={redaction}", message)
