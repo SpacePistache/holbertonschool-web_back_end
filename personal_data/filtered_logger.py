f53dbc0e27c2eaf606bdf88e620d5860f8f0bc26
SpacePistache <lukeholley@hotmail.com>
2026-02-26 09:42:23 +0100

    added type specifiers to function args

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """A module that uses regex to filter out unnecessary data"""
 import re
+from typing import List
 
-def filter_datum(fields, redaction, message, seperator):
+def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
 	""" Function to select and return key information"""
 	filtered_data = rf"({'|'.join(fields)})=[^{seperator}]*"
 	return re.sub(filtered_data, rf"\1={redaction}", message)
