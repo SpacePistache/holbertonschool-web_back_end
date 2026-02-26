6627bf9831f591a5e3480e793a7dcff3f327cdb0
SpacePistache <lukeholley@hotmail.com>
2026-02-26 12:51:07 +0100

    Task 3 first commit

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """A module that uses regex to filter out unnecessary data"""
+import os
+import mysql.connector
 import re
 import logging
 from typing import List
@@ -46,3 +48,19 @@ def get_logger() -> logging.Logger:
     logger.addHandler(handler)
 
     return logger
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connect secure database to read users table."""
+    
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+    
+    conn = mysql.connector.connect(
+        user = username,
+        password = password,
+        host = host,
+        database = database
+    )
+    return conn
