1392d4d6aa3b7680971860101f6071118f297c1c
SpacePistache <lukeholley@hotmail.com>
2026-02-26 14:25:08 +0100

    first commit task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -17,3 +17,17 @@ def hash_password(password: str) -> bytes:
     salty = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salty)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bytes:
+    """
+    Comparing hashed password against password
+
+    Args:
+        hashed password in bytes and the password in str
+
+    Return:
+        Boolean value, true if the password matches, false otherwise.
+    """
+
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
