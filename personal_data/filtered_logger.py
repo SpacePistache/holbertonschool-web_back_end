61d71694443f5a17a2b89c7f1cc1fccd43232412
SpacePistache <lukeholley@hotmail.com>
2026-02-26 10:46:58 +0100

    added missing func documentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -21,10 +21,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """Class initialisation"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Build string with correct format"""
         message = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             message, self.SEPARATOR)
