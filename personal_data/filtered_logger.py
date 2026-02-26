9ede0fb460bc81a6495e91c05a2e2014b3327d13
SpacePistache <lukeholley@hotmail.com>
2026-02-26 10:28:02 +0100

    task 1 added class

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -9,3 +9,21 @@ def filter_datum(fields: List[str], redaction: str,
     """ Function to select and return key information"""
     filtered_data = rf"({'|'.join(fields)})=[^{separator}]*"
     return re.sub(filtered_data, rf"\1={redaction}", message)
+
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
