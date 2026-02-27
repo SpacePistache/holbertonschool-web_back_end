f0178fa2dac1eb48eb594d1b1fb9dd62d91a493e
SpacePistache <lukeholley@hotmail.com>
2026-02-27 15:08:00 +0100

    fixed pycodestyle + added missing return

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,18 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic auth class"""
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Return Base64 auth header"""
+
+        if authorization_header is None:
+            return None
+
+        if not isinstance(self.authorization_header, str):
+            return None
+
+        if not self.authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header.split("Basic ")[1]
