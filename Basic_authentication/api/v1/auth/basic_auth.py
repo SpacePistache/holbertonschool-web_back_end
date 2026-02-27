e1ef82a4b6ab2f485cea8246332855772e692a32
SpacePistache <lukeholley@hotmail.com>
2026-02-27 15:50:12 +0100

    fixed silly pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,7 +53,7 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
 
-        user_email = decoded_base64_authorization_header.split(':', 1)
-        user_pwd = decoded_base64_authorization_header.split(':', 1)
+        user_email, user_pwd = \
+            decoded_base64_authorization_header.split(':', 1)
 
         return user_email, user_pwd
