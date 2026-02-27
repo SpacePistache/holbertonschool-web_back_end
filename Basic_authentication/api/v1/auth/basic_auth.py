a9fac2c33666a2c2a1174e186ccaee16e0d59ade
SpacePistache <lukeholley@hotmail.com>
2026-02-27 14:50:13 +0100

    task 6

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A module that imports from auth.py to create a basic authentication"""
+
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """Basic auth class"""
+    pass
