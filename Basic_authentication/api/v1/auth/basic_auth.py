b66c2913a557a53ca5b62cff0d84fe70b1021d77
SpacePistache <lukeholley@hotmail.com>
2026-02-27 16:01:50 +0100

    task finale

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -83,3 +83,25 @@ class BasicAuth(Auth):
                 return user
 
         return None
+
+    def current_user(self, request=None):
+        """Retrieve User instance for a request"""
+
+        authorization_header = self.authorization_header(request)
+
+        base64_header = self.extract_base64_authorization_header(
+            authorization_header
+        )
+
+        decoded_header = self.decode_base64_authorization_header(
+            base64_header
+        )
+
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_header
+        )
+
+        return self.user_object_from_credentials(
+            user_email,
+            user_pwd
+        )
