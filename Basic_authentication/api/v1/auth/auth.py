ac596a1a78eb03dffc2b3b55cdeff1b99cfd2375
SpacePistache <lukeholley@hotmail.com>
2026-02-27 13:23:57 +0100

    fixed pycodestyle

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -6,22 +6,20 @@ from typing import List, TypeVar
 
 User = TypeVar('User')
 
+
 class Auth():
     """Authentication blueprint"""
-    
-    def __init__(self):
-            pass
 
+    def __init__(self):
+        pass
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """False for now"""
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """None for now"""
         return None
-
 
     def current_user(self, request=None) -> User:
         """None for now"""
