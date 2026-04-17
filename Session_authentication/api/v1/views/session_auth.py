7e46ab0d61c48be3bd877f263bcc5e10a1cf52d6
SpacePistache <lukeholley@hotmail.com>
2026-04-17 14:26:57 +0200

    modified wrong file => fixed

---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Session authentication views """
+from api.v1.views import app_views
+from flask import request, jsonify
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """login route for sesh auth"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or email == "":
+        return jsonify({"error: email missing"}), 404
+
+    if password is None or password == "":
+        return jsonify({"error: wrong password"}), 401
+
+    users = User.search({'email': email})
+    if not users:
+        return jsonify({"error: no user found with this email."}), 404
+
+    user = users[0]
+
+    if not user.is_valid_password(password):
+        return jsonify({"error: incorrect password"}), 401
+
+    from api.v1.app import auth
+
+    session_id = auth.create_session(user.id)
+
+    response = jsonify(user.to_json())
+
+    session_name = getenv("SESSION_NAME")
+    response.set_cookie(session_name, session_id)
+
+    return response
