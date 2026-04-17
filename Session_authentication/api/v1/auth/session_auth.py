57951332b5583afab6c7b097b6b8cdb33f75e5d5
SpacePistache <lukeholley@hotmail.com>
2026-04-17 14:18:27 +0200

    session auth task 7

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,9 @@
 from api.v1.auth.auth import Auth
 from models.user import User
 import uuid
+from os import getenv
+from flask import request, jsonify
+from api.v1.views import app_views
 
 
 class SessionAuth(Auth):
@@ -41,3 +44,34 @@ class SessionAuth(Auth):
             return None
 
         return User.get(user_id)
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """login route for sesh auth"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or email == "":
+        return jsonify({"error: email missing"}), 404
+
+    if password is None or password == "":
+        return jsonify({"error: wrong password"}), 401
+
+    users = User.search({'email': email})
+    if not users:
+        return jsonify({"error: no user found with this email."}), 404
+
+    user = users[0]
+
+    from api.v1.app import auth
+
+    session_id = auth.create_session(user.id)
+
+    response = jsonify(user.to_json())
+
+    session_name = getenv("SESSION_NAME")
+    response.set_cookie(session_name, session_id)
+
+    return response
