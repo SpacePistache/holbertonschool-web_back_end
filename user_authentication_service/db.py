9975113b97a7a5224de863caada35014c9591d0a
SpacePistache <lukeholley@hotmail.com>
2026-04-17 15:56:56 +0200

    hotfix

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -29,13 +29,15 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str):
         """Add a new user to the database"""
 
         user = User(email=email, hashed_password=hashed_password)
 
         self._session.add(user)
         self._session.commit()
+
+        return user
 
     def find_user_by(self, **kwargs):
         """Find a user by attributes"""
