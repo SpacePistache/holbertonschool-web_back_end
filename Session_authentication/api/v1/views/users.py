ad6abbf44178b3c2f7b8a2fa30d1d1aa188471ab
SpacePistache <lukeholley@hotmail.com>
2026-04-17 11:09:50 +0200

    pycodestyle fix

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -27,16 +27,16 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    
+
     if user_id == "me":
         if request.current_user is None:
             abort(404)
         return jsonify(request.current_user.to_json())
-    
+
     user = User.get(user_id)
     if user is None:
         abort(404)
-    
+
     return jsonify(user.to_json())
 
 
@@ -70,6 +70,7 @@ def create_user() -> str:
       - User object JSON represented
       - 400 if can't create the new User
     """
+
     rj = None
     error_msg = None
     try:
