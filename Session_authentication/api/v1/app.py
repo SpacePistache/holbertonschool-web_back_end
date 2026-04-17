d1b5f46ad4c75de2bbf2482008af82e9862d9523
SpacePistache <lukeholley@hotmail.com>
2026-04-17 12:30:10 +0200

    fixed pycodestyle

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -25,7 +25,7 @@ if auth_type == 'auth':
 elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth_type =='session_auth':
+elif auth_type == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
