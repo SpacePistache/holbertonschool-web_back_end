b4748d97eaf2c7b625a849656eddf6facb5684f7
SpacePistache <lukeholley@hotmail.com>
2026-04-17 13:01:38 +0200

    added missing comments

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -10,6 +10,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """create session for id"""
         if user_id is None or type(user_id) is not str:
             return None
 
@@ -18,6 +19,7 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """create user id fpr sesion"""
         if session_id is None or type(session_id) is not str:
             return None
 
