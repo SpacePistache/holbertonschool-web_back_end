af6a4363ef40a5b37e08b98eab77c80aef415efd
SpacePistache <lukeholley@hotmail.com>
2026-04-17 12:57:51 +0200

    added user session mgmt

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -10,15 +10,15 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ Creates a Session ID for a user_id """
-
-        if user_id is None:
-            return None
-        if type(user_id) is not str:
+        if user_id is None or type(user_id) is not str:
             return None
 
         session_id = str(uuid.uuid4())
-
         self.user_id_by_session_id[session_id] = user_id
-
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if session_id is None or type(session_id) is not str:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
