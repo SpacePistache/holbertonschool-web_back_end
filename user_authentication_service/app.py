8c927ce8bb2366a02a86ee6b2b16cde0544010b5
SpacePistache <lukeholley@hotmail.com>
2026-04-20 15:18:09 +0200

    first commit app.py

---
+++ user_authentication_service/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""App module"""
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """Home route"""
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """Register a user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
