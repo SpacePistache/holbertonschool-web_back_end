f39f8ea1a12a204588d8f2102e76311f41f4749c
SpacePistache <lukeholley@hotmail.com>
2026-04-20 15:37:46 +0200

    added uuid

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -34,3 +35,7 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+        
+def _generate_uuid() -> str:
+    """Generate uuid and return its string represenatation"""
+    return str(uuid.uuid4())
