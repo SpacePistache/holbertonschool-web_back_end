4e9be292e62115d85c1023cd1068172b19544928
SpacePistache <lukeholley@hotmail.com>
2026-04-21 09:03:59 +0200

    task 11 initial

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """App module"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -25,6 +25,21 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """User login"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
