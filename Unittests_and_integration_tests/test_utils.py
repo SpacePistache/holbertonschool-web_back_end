a043ab81e4eb9dfb17b3b42039b8b7e3ebd0faa4
SpacePistache <lukeholley@hotmail.com>
2026-04-23 09:23:30 +0200

    fixed missing argument in decorator

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -8,9 +8,9 @@ from parameterized import parameterized
 class TestAccessNestedMap(unittest.TestCase):
 
 	@parameterized.expand([
-		({"a": 1}, ("a",)),
-		({"a": {"b": 2}}, ("a",)),
-		({"a": {"b": 2}}, ("a", "b"),)
+		({"a": 1}, ("a",), 1),
+		({"a": {"b": 2}}, ("a",), {"b": 2}),
+		({"a": {"b": 2}}, ("a", "b"), 2)
 		])
 
 	def test_access_nested_map(self, nested_map, path, expected):
