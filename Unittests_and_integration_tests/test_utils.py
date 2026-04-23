485b600414a2263737de981883987e856185a85b
SpacePistache <lukeholley@hotmail.com>
2026-04-23 10:19:33 +0200

    fixed mismatching arguments and wrong string usage

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -20,7 +20,7 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
         ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
+        ({"a": 1}, ("a", "b"))
     ])
     
     def test_access_nested_map_exception(self, nested_map, path):
@@ -28,4 +28,4 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
 
-        self.assertEqual(str(context.exception), str(path[-1]))
+        self.assertEqual(context.exception.args[0], path[-1])
