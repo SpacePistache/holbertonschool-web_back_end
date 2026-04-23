b51dcdad2ae47e656d88c8f5df37dabce73d12ab
SpacePistache <lukeholley@hotmail.com>
2026-04-23 10:04:41 +0200

    added keyerror testing

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -3,17 +3,29 @@
 
 import unittest
 from utils import access_nested_map
-from parameterized import parameterized 
+from parameterized import parameterized
+
 
 class TestAccessNestedMap(unittest.TestCase):
 
-	@parameterized.expand([
-		({"a": 1}, ("a",), 1),
-		({"a": {"b": 2}}, ("a",), {"b": 2}),
-		({"a": {"b": 2}}, ("a", "b"), 2)
-		])
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test normal cases"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
-	def test_access_nested_map(self, nested_map, path, expected):
-		"""Testing multiple elements"""
-		self.assertEqual(access_nested_map(nested_map, path), expected)
-		return nested_map, path, expected
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test that KeyError is raised"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), str(path[-1]))
