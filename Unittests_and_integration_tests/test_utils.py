7e4edf96bdec7e9855a95fbc4dbb37a10f12f8ca
SpacePistache <lukeholley@hotmail.com>
2026-04-23 13:57:32 +0200

    added http mock test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,9 @@
 
 import unittest
 from utils import access_nested_map
+from utils import get_json
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -13,7 +15,6 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    
     def test_access_nested_map(self, nested_map, path, expected):
         """Test normal cases"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
@@ -22,10 +23,24 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
     ])
-    
     def test_access_nested_map_exception(self, nested_map, path):
         """Test that KeyError is raised"""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
 
         self.assertEqual(context.exception.args[0], path[-1])
+
+
+class TestGetJson(unittest.TestCase):
+    """Use Mocking to test HTTP calls"""
+
+    @patch("utils.requests.get")
+    def test_get_json(self, mock_get):
+
+        mock_http_call = Mock()
+        mock_http_call.json.return_value = {"key": "value"}
+        mock_get.return_value = mock_http_call
+
+        result = get_json("http://example.com")
+
+        self.assertEqual(result, {"key": "value"})
