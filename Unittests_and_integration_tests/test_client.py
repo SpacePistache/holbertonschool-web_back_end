25be0e5ffaa3db65ba0f1616b38ef46bc663ea77
SpacePistache <lukeholley@hotmail.com>
2026-04-24 11:15:15 +0200

    fixed tabs to space indentation

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -4,29 +4,46 @@
 from utils import get_json
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-	"""test GithubOrgClient"""
-	
-	@parameterized.expand([
-		("google",),
-		("abc",),
-	])
-	@patch("client.get_json")
-	def test_org(self, org_name, mock_get_json):
-		"""assures org returns correct value"""
+    """test GithubOrgClient"""
 
-		expected = {"login": org_name}
-		mock_get_json.return_value = expected
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name, mock_get_json):
+        """assures org returns correct value"""
 
-		client = GithubOrgClient(org_name)
-		result = client.org
+        expected = {"login": org_name}
+        mock_get_json.return_value = expected
 
-		self.assertEqual(result, expected)
+        client = GithubOrgClient(org_name)
+        result = client.org
 
-		mock_get_json.assert_called_once_with(
-			f"https://api.github.com/orgs/{org_name}"
-		)
+        self.assertEqual(result, expected)
+
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
+
+    def test_public_repos_url(self):
+        """Test public repos"""
+
+        payload = {"repos_url": "https://api.github.com/orgs/test/repos"}
+
+        with patch(
+            "client.GithubOrgClient.org",
+            new_callable=PropertyMock
+        ) as mock_org:
+
+            mock_org.return_value = payload
+
+            client = GithubOrgClient("test")
+            result = client._public_repos_url
+
+            self.assertEqual(result, payload["repos_url"])
