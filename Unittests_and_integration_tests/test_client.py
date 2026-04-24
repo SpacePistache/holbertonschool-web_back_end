11a8a87636aabf0b12b3a7871609d85d522e7e71
SpacePistache <lukeholley@hotmail.com>
2026-04-24 11:18:45 +0200

    test client public repos additon

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -47,3 +47,31 @@ class TestGithubOrgClient(unittest.TestCase):
             result = client._public_repos_url
 
             self.assertEqual(result, payload["repos_url"])
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """Test public_repos returns expected list"""
+
+        payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"},
+        ]
+
+        mock_get_json.return_value = payload
+
+        with patch.object(
+            GithubOrgClient,
+            "_public_repos_url",
+            new_callable=PropertyMock
+        ) as mock_url:
+
+            mock_url.return_value = "https://fake-url.com"
+
+            client = GithubOrgClient("test")
+            result = client.public_repos()
+
+            self.assertEqual(result, ["repo1", "repo2", "repo3"])
+
+            mock_get_json.assert_called_once_with("https://fake-url.com")
+            mock_url.assert_called_once()
