d74285fbaeb5d751a5e967bb59d3dbf813d10e39
SpacePistache <lukeholley@hotmail.com>
2026-06-19 10:31:01 +0200

    fix broken uuid key and wrong self declaration for flush

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -12,11 +12,11 @@ class Cache:
     def __init__(self):
         """stores instance of private variable then flushes it."""
         self._redis = redis.Redis()
-        self._redis = flushdb()
+        self._redis.flushdb()
 
     def store(self, data: str | bytes | int | float) -> str:
         """generates a random key and stores input data"""
-        key = str(uuid(4))
+        key = str(uuid.uuid4())
 
         self._redis.set(key, data)
 
