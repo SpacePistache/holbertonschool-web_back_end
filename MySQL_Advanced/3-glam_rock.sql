3727666ee1d1375cd785f699c4d8bae13cfc3d06
SpacePistache <lukeholley@hotmail.com>
2026-06-22 15:40:10 +0200

    fixed indentation

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,10 @@
 --List groups categorized as glam rock and their longevity
+
 SELECT band_name,
-	CASE
-		WHEN split IS NULL THEN 2024 - formed
-		ELSE split -formed
-	END AS lifespan
+       CASE
+           WHEN split IS NULL THEN 2024 - formed
+           ELSE split - formed
+       END AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
