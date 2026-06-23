0835c52200cd14774ae7747c42583cd5c26b135d
SpacePistache <lukeholley@hotmail.com>
2026-06-23 10:56:23 +0200

    first commit task 4

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- Script updating quantities after a placed order.
+
+CREATE TRIGGER decrease_item_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name
