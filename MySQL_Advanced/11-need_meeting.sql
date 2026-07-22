f32585bfcbfa0590eaf3f4a75acc3e87f2973775
SpacePistache <lukeholley@hotmail.com>
2026-07-22 10:47:52 +0200

    t11 initial

---
+++ MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,10 @@
+-- Creates a view listing students who need a meeting
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+  AND (
+        last_meeting IS NULL
+        OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+      );
