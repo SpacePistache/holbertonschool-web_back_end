d7b8510baff65a39b2c7e68901259f5de7f51375
SpacePistache <lukeholley@hotmail.com>
2026-07-22 10:46:38 +0200

    T10 inital commit

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- Creates a function that safely divides two integers
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+
+    RETURN a / b;
+END$$
+
+DELIMITER ;
